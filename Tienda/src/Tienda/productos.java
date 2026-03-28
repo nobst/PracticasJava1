d76d09a41938b604de64b7103c12bbc73c172dca
christian <christian.pl1994@gmail.com>
2026-03-28 00:14:36 -0500

    creacion clase productos

--- Tienda/src/Tienda/productos.java
+++ Tienda/src/Tienda/productos.java
@@ -1,5 +1,58 @@
 package Tienda;
 
 public class productos {
+	
+	private String Nombre;
+	private double PrecioVenta;
+	private int CantidadStock;
+	private int Umbral;
 
+	
+	private productos(String Nombre, double PrecioVenta, int CantidadStock) {
+		this.Nombre = Nombre;
+		this.PrecioVenta = PrecioVenta;
+		
+		
+		if(CantidadStock<0){
+			this.CantidadStock = 0;
+		}
+		else {
+			this.CantidadStock = CantidadStock;
+		}	
+	}
+
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+
+	public double getPrecioVenta() {
+		return PrecioVenta;
+	}
+
+
+	public void setPrecioVenta(double precioVenta) {
+		PrecioVenta = precioVenta;
+	}
+
+
+	public int getCantidadStock() {
+		return CantidadStock;
+	}
+
+
+	public void setCantidadStock(int cantidadStock) {
+		CantidadStock = cantidadStock;
+	}
+	
+	
+	
 }
+
+
