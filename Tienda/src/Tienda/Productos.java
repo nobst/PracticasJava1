01fb75d5e40e1e21bddd6444d3cbb07e4276290e
danie <danie@Christian_plaza>
2026-03-28 01:11:33 -0500

    cambio en las clases

---
+++ Tienda/src/Tienda/Productos.java
@@ -0,0 +1,113 @@
+package Tienda;
+
+public class Productos {
+	private int id;
+	private String nombre;
+	private double precio;
+	private int stock;
+	private Categoria categoria;
+	private Proveedor proveedor;
+	
+	
+	
+	private Productos (int id, String nombre, double precio, int stock,
+						Categoria categoria, Proveedor proveedor) {
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.stock = stock;
+		this.categoria = categoria;
+		this.proveedor = proveedor;
+		
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public double getPrecio() {
+		return precio;
+	}
+
+
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+
+
+	public int getStock() {
+		return stock;
+	}
+
+
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+
+
+	public Proveedor getProveedor() {
+		return proveedor;
+	}
+
+
+
+	public void setProveedor(Proveedor proveedor) {
+		this.proveedor = proveedor;
+	}
+
+
+		
+	 public void descontarStock(int cantidad) {
+	        if (stock < cantidad) {
+	            throw new IllegalArgumentException("Stock insuficiente");
+	        }
+	        stock -= cantidad;
+	    }
+
+	
+
+
+	
+	
+	
+}
+
+
